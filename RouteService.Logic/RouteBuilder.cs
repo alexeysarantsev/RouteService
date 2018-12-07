1b8074e13086ca2a8f95aec475926f26f4469f5f
alexey.sarantsev <alexey.sarantsev@totalobjects.spb.ru>
2018-12-07 19:16:43 +0300

    Add RouteService.Logic project

---
+++ RouteService.Logic/RouteBuilder.cs
@@ -0,0 +1,24 @@
+﻿using RouteService.Model.Interfaces;
+using System;
+
+namespace RouteService.Logic
+{
+    public class RouteBuilder
+    {
+        private readonly IAirlineProvider _airlineProvider;
+        private readonly IAirportProvider _airportProvider;
+        private readonly IRouteProvider _routeProvider;
+
+        public RouteBuilder(IAirlineProvider airlineProvider, IAirportProvider airportProvider, IRouteProvider routeProvider)
+        {
+            _airlineProvider = airlineProvider ?? throw new ArgumentNullException(nameof(airlineProvider));
+            _airportProvider = airportProvider ?? throw new ArgumentNullException(nameof(airportProvider));
+            _routeProvider = routeProvider ?? throw new ArgumentNullException(nameof(routeProvider));
+        }
+
+        public void Build(string sourceAirport, string destinationAirport)
+        {
+
+        }
+    }
+}
