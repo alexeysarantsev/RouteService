777d8359cf780098a17f2df4945fde324fde265a
alexey.sarantsev <alexey.sarantsev@totalobjects.spb.ru>
2018-12-07 18:44:47 +0300

    DI added RouteService renamed to RouteService.Api

--- RouteService/Controllers/RouteController.cs
+++ RouteService/Controllers/RouteController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RouteService.Model;
+using RouteService.Model.Interfaces;
 using System;
 using System.Threading.Tasks;
 
@@ -7,13 +9,34 @@ namespace RouteService.Controllers
     [Route("api/[controller]"), Produces("application/json"), ApiController]
     public class RouteController : ControllerBase
     {
-        public RouteController()
+        private readonly IAirlineProvider _airlineProvider;
+        private readonly IAirportProvider _airportProvider;
+        private readonly IRouteProvider _routeProvider;
+        public RouteController(IAirlineProvider airlineProvider, IAirportProvider airportProvider, IRouteProvider routeProvider)
         {
+            _airlineProvider = airlineProvider;
+            _airportProvider = airportProvider;
+            _routeProvider = routeProvider;
         }
 
+        /// <summary>
+        /// Searches for a route from the sourceAirport to the destinationAirport.
+        /// </summary>
+        /// <param name="sourceAirport">The name or the code of the source airport.</param>
+        /// <param name="destinationAirport">The name or the code of the destination airport.</param>
+        /// <returns>The route.</returns>
         [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] string from, [FromQuery] string to)
+        [ProducesResponseType(typeof(Route), 200)]
+        [ProducesResponseType(typeof(void), 400)]
+        [ProducesResponseType(typeof(void), 404)]
+        [ProducesResponseType(typeof(void), 500)]
+        /// <response code="200">The route is found.</response>
+        /// <response code="400">The source of destination airport is not set or not found.</response>
+        /// <response code="404">The route is not found.</response>
+        /// <response code="500">Unhandled server error.</response>
+        public async Task<IActionResult> Get([FromQuery] string sourceAirport, [FromQuery] string destinationAirport)
         {
+            await _airportProvider.Get(sourceAirport);
             return Ok();
         }
     }
--- RouteService/Startup.cs
+++ RouteService/Startup.cs
@@ -6,18 +6,46 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using RouteService.FlightsServiceProvider;
+using RouteService.Model.Interfaces;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace RouteService
 {
     public class Startup
     {
+        private const string flightsServiceUrlKey = "FlightsServiceUrl";
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        private IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string flightsServiceUrl = Configuration[flightsServiceUrlKey];
+            var flightsservice = new FlightsServiceClient.Flightsservice(new Uri(flightsServiceUrl));
+
+            var airlineProvider = new AirlineProvider(flightsservice);
+            var routeProvider = new RouteProvider(flightsservice);
+            var airportProvider = new AirportProvider(flightsservice);
+
+            //TODO: check lifecycle 
+            services.AddSingleton<IAirlineProvider>(airlineProvider);
+            services.AddSingleton<IRouteProvider>(routeProvider);
+            services.AddSingleton<IAirportProvider>(airportProvider);
+
             services
                .AddMvc(options =>
                {
