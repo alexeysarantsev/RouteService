c16ea394861bbe9924b8efd802f87c384c4d2a8b
alexey.sarantsev <alexey.sarantsev@totalobjects.spb.ru>
2018-12-07 14:10:44 +0300

    Add an empty site with swagger screen

---
+++ RouteService/Controllers/RouteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace RouteService.Controllers
+{
+    [Route("api/[controller]"), Produces("application/json"), ApiController]
+    public class RouteController : ControllerBase
+    {
+        public RouteController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string from, [FromQuery] string to)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ RouteService/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace RouteService
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+               .AddMvc(options =>
+               {
+                    //options.Conventions.Add(new CommaSeparatedQueryStringConvention());
+                })
+               .AddJsonOptions(options =>
+               {
+                   options.SerializerSettings.Formatting = Formatting.Indented;
+               })
+               .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Route API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Alexey Sarantsev",
+                        Email = "alexey.sarantsev@gmail.com"
+                    }
+                });
+
+                // set the comments path for the Swagger JSON and UI
+                foreach (string xmlDocument in System.IO.Directory.EnumerateFiles(AppContext.BaseDirectory, "*.xml"))
+                {
+                    options.IncludeXmlComments(xmlDocument, includeControllerXmlComments: true);
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Route Service API v1");
+                options.RoutePrefix = string.Empty;
+            });
+
+            app.UseCors(_ => _.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseMvc();
+        }
+    }
+}
