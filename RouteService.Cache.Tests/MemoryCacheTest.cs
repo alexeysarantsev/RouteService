e1e22e06160263b26365c78fd80f93b7f3076ed8
alexey.sarantsev <alexey.sarantsev@gmail.com>
2018-12-09 01:03:07 +0300

    RouteService.Cache tests added

---
+++ RouteService.Cache.Tests/MemoryCacheTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using RouteService.Cache;
+
+namespace RouteService.Cache.Tests
+{
+    [TestFixture]
+    public class MemoryCacheTests
+    {
+        [Test]
+        public async Task GetOrCreateAsyncFactoryCalledTest()
+        {
+            MemoryCache<string, string> cache = new MemoryCache<string, string>(TimeSpan.FromHours(1));
+            int callsCount = 0;
+            var value = await cache.GetOrCreateAsync("key", key => { callsCount++; return Task.FromResult("value"); });
+            Assert.AreEqual(1, callsCount);
+            Assert.AreEqual("value", value);
+        }
+
+        [Test]
+        public async Task GetOrCreateAsyncValueCachedTest()
+        {
+            MemoryCache<string, string> cache = new MemoryCache<string, string>(TimeSpan.FromHours(1));
+            int callsCount = 0;
+            await cache.GetOrCreateAsync("key", key => { callsCount++; return Task.FromResult("value"); });
+            var value = await cache.GetOrCreateAsync("key", key => { callsCount++; return Task.FromResult("value 2"); });
+            Assert.AreEqual(1, callsCount);
+            Assert.AreEqual("value", value);
+        }
+
+        [Test]
+        public async Task GetOrCreateAsyncTimespanTest()
+        {
+            MemoryCache<string, string> cache = new MemoryCache<string, string>(TimeSpan.FromTicks(0));
+            int callsCount = 0;
+            await cache.GetOrCreateAsync("key", key => { callsCount++; return Task.FromResult("value"); });
+            var value = await cache.GetOrCreateAsync("key", key => { callsCount++; return Task.FromResult("value 2"); });
+            Assert.AreEqual(2, callsCount);
+            Assert.AreEqual("value 2", value);
+        }
+
+    }
+}
