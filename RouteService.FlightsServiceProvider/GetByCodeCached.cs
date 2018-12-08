f52f902ce04b437e3a25c3306bffd926c6a7fdbe
alexey.sarantsev <alexey.sarantsev@gmail.com>
2018-12-08 21:28:13 +0300

    Add missed file

---
+++ RouteService.FlightsServiceProvider/GetByCodeCached.cs
@@ -0,0 +1,23 @@
+﻿using RouteService.Model.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace RouteService.FlightsServiceProvider
+{
+    public class GetByCodeCached<TValue> : IGetByAlias<TValue>
+    {
+        private Cache.MemoryCache<string, TValue> _cache;
+        private Func<string, Task<TValue>> _factory;
+
+        public GetByCodeCached(TimeSpan ttl, Func<string, Task<TValue>> factory)
+        {
+            _cache = new Cache.MemoryCache<string, TValue>(ttl);
+            _factory = factory;
+        }
+
+        public Task<TValue> Get(string alias)
+        {
+            return _cache.GetOrCreateAsync(alias, _factory);
+        }
+    }
+}
