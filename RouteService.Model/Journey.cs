91f408277454555b0f2538425ca4ed08e3d8ccb7
alexey.sarantsev <alexey.sarantsev@gmail.com>
2018-12-10 00:30:32 +0300

    some comments added

--- RouteService.Model/Journey.cs
+++ RouteService.Model/Journey.cs
@@ -3,8 +3,14 @@ using RouteService.FlightsServiceClient.Models;
 
 namespace RouteService.Model
 {
+    /// <summary>
+    /// Represens a journey contains one or more routes
+    /// </summary>
     public class Journey
     {
+        /// <summary>
+        /// A routes array
+        /// </summary>
         public Route[] Routes { get; set; }
     }
 }
